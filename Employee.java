9842ce3fba9d8e7e122b4355aee708ad725fbee6
Nyein Nyein Aye <allisonnyein@gmail.com>
2025-07-04 11:36:48 -0700

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,43 +1,57 @@
 package Company;
 
+import java.util.List;
+
 public class Employee {
     public int id;
     public String name;
-    public String role;
+    public List<String> roles;
     private int leaveBalance;
 
-    public Employee(int id, String name, String role, int leaveBalance){
+    public Employee(int id, String name, List<String> roles, int leaveBalance){
         this.id = id;
         this.name = name;
-        this.role = role;
+        this.roles = roles;
         this.leaveBalance = leaveBalance;
     }
-    public int getId() {return id;}
-    public String getName() {return name;}
-    public String getRole() {return role;}
-    public int getLeaveBalance(){ return leaveBalance;}
+
+    public int getId() { return id; }
+
+    public String getName() { return name; }
+
+    public List<String> getRoles() { return roles; }
+
+    public int getLeaveBalance() { return leaveBalance; }
 
     public void setName(String name){
-       this.name = name;
+        this.name = name;
     }
 
-    public void setRole (String role){
-        this.role = role;
+    public void setRole(List<String> role){
+        this.roles = role;
     }
 
     public void setLeaveBalance(int leaveBalance){
-        this.leaveBalance= leaveBalance;
+        this.leaveBalance = leaveBalance;
     }
 
     public void deductLeave(int days){
         this.leaveBalance -= days;
     }
 
+    public boolean hasRoles(String role){
+        return roles != null && roles.contains(role);
+    }
+
+    public boolean hasSoDConflict(){
+        return hasRoles("Processor") && hasRoles("Approver");
+    }
+
     @Override
     public String toString() {
         return "Employee{" + "id=" + id +
                 ", name='" + name + '\'' +
-                ", role='" + role + '\'' +
+                ", role='" + roles + '\'' +
                 ", leaveBalance=" + leaveBalance +
                 '}';
     }
